2379a17dddf908795c30635b895ebeafa5dbefea
jasonh34 <105529595+jasonh34@users.noreply.github.com>
2023-06-21 18:40:34 -0400

    refactor: rename component to be multi-word

--- src/components/LocationSearchbar.vue
+++ src/components/LocationSearchbar.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
 import { ref } from 'vue';
-import { storeToRefs } from 'pinia';
 import { useWeatherStore } from '@/stores/weather';
 import router from '@/router';
 const weatherStore = useWeatherStore();
