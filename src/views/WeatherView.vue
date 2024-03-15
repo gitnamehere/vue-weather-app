ba3986364f40552ee900d1015d77387d30fc01fb
gitrnamehere <105529595+gitnamehere@users.noreply.github.com>
2024-03-14 22:18:42 -0400

    Update WeatherView.vue

--- src/views/WeatherView.vue
+++ src/views/WeatherView.vue
@@ -21,8 +21,7 @@ const { weather, temperatureUnit } = storeToRefs(weatherStore);
     >
         <div class="weather__top-bar">
             <a href="/">
-                <h1>Insert Title Here
-                </h1>
+                <h1>A Vue Weather App</h1>
             </a>
             <Searchbar class="weather__search-bar" />
             <button class="weather__menu-button" @click="weatherStore.toggleTemperatureUnit">
