097ad97aa6b6829f5c8ebf8dcf04ca920c2007eb
gitrnamehere <105529595+gitnamehere@users.noreply.github.com>
2024-06-17 21:33:36 -0400

    Create HumidityCard.vue

---
+++ src/components/weather/HumidityCard.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+import CardItem from '@/components/CardItem.vue';
+
+defineProps<{
+    humidity: number;
+}>();
+</script>
+
+<template>
+    <CardItem
+        class="humidity-card"
+        header="Humidity (WIP)"
+        :square="true"
+    >
+        <div class="humidity-card__body">
+            <h1 class="humidity-card__indicator">
+                {{ humidity }}%
+            </h1>
+        </div>
+    </CardItem>
+</template>
+
+<style lang="scss">
+    .humidity-card {
+        &__body {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            flex-direction: column;
+            height: 100%;
+        }
+
+        &__indicator {
+            font-size: 300%;
+        }
+    }
+</style>
