e8b330e289a54bf1a828598d795627e1c28dfce5
gitrnamehere <105529595+gitnamehere@users.noreply.github.com>
2024-06-20 21:13:18 -0400

    Update HumidityCard.vue

--- src/components/weather/HumidityCard.vue
+++ src/components/weather/HumidityCard.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import CardItem from '@/components/CardItem.vue';
+import RaindropGauge from '@/components/RaindropGauge.vue';
 
 defineProps<{
     humidity: number;
@@ -9,10 +10,14 @@ defineProps<{
 <template>
     <CardItem
         class="humidity-card"
-        header="Humidity (WIP)"
+        header="Humidity"
         :square="true"
     >
         <div class="humidity-card__body">
+            <RaindropGauge
+                class="humidity-card__raindrop"
+                :progress="`${humidity}%`"
+            />
             <h2 class="humidity-card__indicator">
                 {{ humidity }}%
             </h2>
@@ -28,6 +33,12 @@ defineProps<{
             align-items: center;
             flex-direction: column;
             height: 100%;
+        }
+
+        &__raindrop {
+            height: 50%;
+            width: 50%;
+            margin: 8px auto;
         }
     }
 </style>
