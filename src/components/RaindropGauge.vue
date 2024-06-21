fa5107f3a3e5f7e4ea55b1d978d384ee01195caa
gitrnamehere <105529595+gitnamehere@users.noreply.github.com>
2024-06-20 21:13:08 -0400

    Create RaindropGauge.vue

---
+++ src/components/RaindropGauge.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+defineProps<{progress: string}>();
+</script>
+
+<template>
+    <div class="raindrop">
+        <div class="raindrop__water" />
+    </div>
+</template>
+
+<style lang="scss">
+    .raindrop {
+        display: flex;
+        background-color: #AAA2;
+        mask-size: 100% 100%;
+        mask-repeat: no-repeat;
+        mask-image: url("@/assets/weather-icons/svg/wi-raindrop.svg");
+
+        &__water {
+            margin-top: auto;
+            height: v-bind(progress);
+            width: 100%;
+            background-color: #FFF;
+
+            transition: height 0.5s ease;
+        }
+    }
+</style>
